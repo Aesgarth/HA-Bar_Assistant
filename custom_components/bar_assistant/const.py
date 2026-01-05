2d80deec9dea47f25526c8d18403e0de6e51e2bf
Aesgarth <getflair@gmail.com>
2026-01-05 21:22:17 +0000

    Add new constants for bar assistant configuration

--- custom_components/bar_assistant/const.py
+++ custom_components/bar_assistant/const.py
@@ -1,6 +1,8 @@
 DOMAIN = "bar_assistant"
 CONF_API_URL = "api_url"
 CONF_API_TOKEN = "api_token"
+CONF_BAR_ID = "bar_id"  # <--- New
 
 # Defaults
 DEFAULT_API_URL = "http://bar-assistant.local/bar"
+DEFAULT_BAR_ID = 1
