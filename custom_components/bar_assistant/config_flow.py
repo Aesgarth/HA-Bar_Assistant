1a77b28363269a339f4e828c763ee40a4d80c277
Aesgarth <getflair@gmail.com>
2026-01-05 21:39:52 +0000

    Add support for sync all users option in config flow

--- custom_components/bar_assistant/config_flow.py
+++ custom_components/bar_assistant/config_flow.py
@@ -1,24 +1,20 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from .const import DOMAIN, CONF_API_URL, CONF_API_TOKEN, CONF_BAR_ID, DEFAULT_API_URL, DEFAULT_BAR_ID
+from .const import DOMAIN, CONF_API_URL, CONF_API_TOKEN, CONF_BAR_ID, CONF_SYNC_ALL_USERS, DEFAULT_API_URL, DEFAULT_BAR_ID, DEFAULT_SYNC_ALL_USERS
 from .api import BarAssistantAPI
 
 class BarAssistantConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Bar Assistant."""
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
         errors = {}
-
         if user_input is not None:
-            # We pass the Bar ID to the API validator now
             api = BarAssistantAPI(
                 user_input[CONF_API_URL], 
                 user_input[CONF_API_TOKEN],
                 user_input.get(CONF_BAR_ID, DEFAULT_BAR_ID)
             )
             valid = await self.hass.async_add_executor_job(api.validate_auth)
-
             if valid:
                 return self.async_create_entry(title="Bar Assistant", data=user_input)
             else:
@@ -29,7 +25,8 @@ class BarAssistantConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             data_schema=vol.Schema({
                 vol.Required(CONF_API_URL, default=DEFAULT_API_URL): str,
                 vol.Required(CONF_API_TOKEN): str,
-                vol.Required(CONF_BAR_ID, default=DEFAULT_BAR_ID): int, # <--- New Field
+                vol.Required(CONF_BAR_ID, default=DEFAULT_BAR_ID): int,
+                vol.Optional(CONF_SYNC_ALL_USERS, default=DEFAULT_SYNC_ALL_USERS): bool, # <--- New Checkbox
             }),
             errors=errors,
         )
